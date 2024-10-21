6c0b720695c4f6a5fb04f1ee8ee623198565d442
Gerten Pilv <gerten.pilv8@gmail.com>
2024-10-21 12:52:18 +0300

    es vs commonjs

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//const fs = require("fs"); commonjs
-import fs from 'fs' // es moodul
-
-fs.writeFileSync('test.txt', 'hello node!');
+let common = require('./common.js')
+common.hello();
+import es from './es.js'
+es.hello();
