7299f2576c37ae86b7cb78371d7ca77c47f6c072
GertenP <gerten.pilv@tptlive.ee>
2025-02-11 11:25:18 +0200

    Add vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,14 @@
 <script setup>
 import { ref } from 'vue';
 import Tabs from './components/Tabs.vue';
-
-import Modals from './pages/Modals.vue';
-import ToDo from './pages/ToDo.vue';
-
-let titles = ['Todo', 'Modals'];
-let contents = [ToDo, Modals];
-let activeTab = ref(0);
-function change(key) {
-  console.log(key);
-  activeTab.value = key;
-}
+import { RouterView } from 'vue-router';
 
 </script>
 
 <template>
-  <Tabs :items="titles" @change="change"></Tabs>
-  <component :is="contents[activeTab]"></component>
+  <Tabs></Tabs>
+  <RouterView></RouterView>
 
-  <footer>Andreas on neeger</footer>
 </template>
 
 <style></style>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
 import './style.scss';
 import { createApp } from 'vue';
-
 import App from './App.vue'
-const app = createApp(App)
+
+import {createWebHashHistory, createMemoryHistory, createRouter } from 'vue-router';
+import ToDo from './pages/ToDo.vue';
+import Modals from './pages/Modals.vue';
+
+const routes = [
+  { path: '/', component: ToDo, name:"Todo"},
+  { path: '/modals', component: Modals, name:"Modals" },
+]
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
+});
+
+const app = createApp(App);
+app.use(router);
 app.mount('#app');
+
