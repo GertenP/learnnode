335a34a8afe486a71b8a9751992c16cd9a6e4507
GertenP <gerten.pilv@tptlive.ee>
2025-02-11 10:30:40 +0200

    Add simple change shower

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,19 @@
 <script setup>
+import { ref } from 'vue';
 import Tabs from './components/Tabs.vue';
-let titles = ['Items1','Items2','Items3','Items4'];
-
+let titles = ['Items1', 'Items2', 'Items3', 'Items4'];
+let contents = ['content 1', 'content 2', 'content 3', 'content 4',];
+let activeTab = ref(0);
+function change(key) {
+  console.log(key);
+  activeTab.value = key;
+}
 
 </script>
 
 <template>
-  <Tabs :items="titles"></Tabs>
+  <Tabs :items="titles" @change="change"></Tabs>
+  <h1>{{ contents[activeTab] }}</h1>
 </template>
 
 <style></style>
